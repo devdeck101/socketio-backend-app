17c93a2dd2c351c756a789de49210a8b301751ba
iagobispo <iagonispo56@gmail.com>
2022-06-18 11:01:14 -0300

    first commit

--- server.js
+++ server.js
@@ -25,3 +25,60 @@ io.on('connection', socket => {
 })
 
 server.listen(PORT, () => console.log(`Listening server on port ${PORT}`))
+
+
+// var WebSocketServer = require('websocket').server;
+// var http = require('http');
+
+// //Porta que o server irá escutar
+// const port = 4000;
+
+// //Cria o server
+// var server = http.createServer();
+
+
+// //Server irá escutar na porta definida em 'port'
+// server.listen(port, () => {
+//     //Server está pronto
+//     console.log(`Server está executando na porta ${port}`);
+// });
+
+// //Cria o WebSocket server
+// wsServer = new WebSocketServer({
+//   httpServer: server
+// });
+
+
+// //Chamado quando um client deseja conectar
+// wsServer.on('request', (request) => {
+//     //Estado do led: false para desligado e true para ligado
+//     let state = false;
+
+//     //Aceita a conexão do client
+//     let client = request.accept(null, request.origin);
+
+//     //Chamado quando o client envia uma mensagem
+//     client.on('message', (message) => {
+//         //Se é uma mensagem string utf8
+//         if (message.type === 'utf8') {
+//             //Mostra no console a mensagem
+//             console.log(message.utf8Data);
+//         }
+//     });
+
+
+//   //Cria uma função que será executada a cada 1 segundo (1000 millis) para enviar o estado do led
+//   let interval = setInterval(() => {
+//     //Envia para o client "ON" ou "OFF" dependendo do estado atual da variável state
+//     client.sendUTF(state? "ON" : "OFF");
+//     //Inverte o estado
+//     state = !state;
+// }, 1000);//Tempo entre chamadas => 1000 millis = 1 segundo 
+
+// //Chamado quando a conexão com o client é fechada
+// client.on('close', () => {
+//     console.log("Conexão fechada");
+//     //Remove o intervalo de envio de estado
+//     clearInterval(interval);
+// });
+// });
